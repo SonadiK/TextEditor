6fb406e267d704bcb64eb1d2a196bc90e2542bfd
Sonadi Kannangara <ksonadi@gmail.com>
2021-03-28 21:35:07 -0400

    Add project files.

---
+++ TextEditor/TextEditor.Designer.cs
@@ -0,0 +1,250 @@
+﻿
+namespace TextEditor
+{
+    partial class formTextEditor
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(formTextEditor));
+            this.menuTop = new System.Windows.Forms.MenuStrip();
+            this.menuFile = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuFileNew = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuFileOpen = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuFileSave = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuFileSaveAs = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuFileExit = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuEdit = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuEditCut = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuEditCopy = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuEditPaste = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuHelp = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuHelpAbout = new System.Windows.Forms.ToolStripMenuItem();
+            this.textBoxInput = new System.Windows.Forms.TextBox();
+            this.toolTip = new System.Windows.Forms.ToolTip(this.components);
+            this.menuTop.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuTop
+            // 
+            this.menuTop.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuTop.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.menuFile,
+            this.menuEdit,
+            this.menuHelp});
+            this.menuTop.Location = new System.Drawing.Point(0, 0);
+            this.menuTop.Name = "menuTop";
+            this.menuTop.Size = new System.Drawing.Size(800, 28);
+            this.menuTop.TabIndex = 0;
+            this.menuTop.Text = "menuStrip1";
+            // 
+            // menuFile
+            // 
+            this.menuFile.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.menuFileNew,
+            this.menuFileOpen,
+            this.menuFileSave,
+            this.menuFileSaveAs,
+            this.menuFileExit});
+            this.menuFile.Name = "menuFile";
+            this.menuFile.Size = new System.Drawing.Size(46, 24);
+            this.menuFile.Text = "&File";
+            this.menuFile.ToolTipText = "Options related the file management.";
+            // 
+            // menuFileNew
+            // 
+            this.menuFileNew.Image = ((System.Drawing.Image)(resources.GetObject("menuFileNew.Image")));
+            this.menuFileNew.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.menuFileNew.Name = "menuFileNew";
+            this.menuFileNew.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.N)));
+            this.menuFileNew.Size = new System.Drawing.Size(181, 26);
+            this.menuFileNew.Text = "&New";
+            this.menuFileNew.ToolTipText = "Create a new file.";
+            this.menuFileNew.Click += new System.EventHandler(this.FileNewClick);
+            // 
+            // menuFileOpen
+            // 
+            this.menuFileOpen.Image = ((System.Drawing.Image)(resources.GetObject("menuFileOpen.Image")));
+            this.menuFileOpen.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.menuFileOpen.Name = "menuFileOpen";
+            this.menuFileOpen.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.O)));
+            this.menuFileOpen.Size = new System.Drawing.Size(181, 26);
+            this.menuFileOpen.Text = "&Open";
+            this.menuFileOpen.ToolTipText = "Click to open a file.";
+            this.menuFileOpen.Click += new System.EventHandler(this.FileOpenClick);
+            // 
+            // menuFileSave
+            // 
+            this.menuFileSave.Image = ((System.Drawing.Image)(resources.GetObject("menuFileSave.Image")));
+            this.menuFileSave.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.menuFileSave.Name = "menuFileSave";
+            this.menuFileSave.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.S)));
+            this.menuFileSave.Size = new System.Drawing.Size(181, 26);
+            this.menuFileSave.Text = "&Save";
+            this.menuFileSave.ToolTipText = "Click to save the current file.";
+            this.menuFileSave.Click += new System.EventHandler(this.FileSaveClick);
+            // 
+            // menuFileSaveAs
+            // 
+            this.menuFileSaveAs.Name = "menuFileSaveAs";
+            this.menuFileSaveAs.Size = new System.Drawing.Size(181, 26);
+            this.menuFileSaveAs.Text = "Save &As";
+            this.menuFileSaveAs.ToolTipText = "Click to save the the file location to save the file.";
+            this.menuFileSaveAs.Click += new System.EventHandler(this.FileSaveAsClick);
+            // 
+            // menuFileExit
+            // 
+            this.menuFileExit.Name = "menuFileExit";
+            this.menuFileExit.Size = new System.Drawing.Size(181, 26);
+            this.menuFileExit.Text = "E&xit";
+            this.menuFileExit.ToolTipText = "Click to exit the application.";
+            this.menuFileExit.Click += new System.EventHandler(this.FileExitClick);
+            // 
+            // menuEdit
+            // 
+            this.menuEdit.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.menuEditCut,
+            this.menuEditCopy,
+            this.menuEditPaste});
+            this.menuEdit.Name = "menuEdit";
+            this.menuEdit.Size = new System.Drawing.Size(49, 24);
+            this.menuEdit.Text = "&Edit";
+            this.menuEdit.ToolTipText = "Options relate to editing.";
+            // 
+            // menuEditCut
+            // 
+            this.menuEditCut.Image = ((System.Drawing.Image)(resources.GetObject("menuEditCut.Image")));
+            this.menuEditCut.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.menuEditCut.Name = "menuEditCut";
+            this.menuEditCut.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.X)));
+            this.menuEditCut.Size = new System.Drawing.Size(177, 26);
+            this.menuEditCut.Text = "Cu&t";
+            this.menuEditCut.ToolTipText = "Click to cut the selected content.";
+            this.menuEditCut.Click += new System.EventHandler(this.EditCutClick);
+            // 
+            // menuEditCopy
+            // 
+            this.menuEditCopy.Image = ((System.Drawing.Image)(resources.GetObject("menuEditCopy.Image")));
+            this.menuEditCopy.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.menuEditCopy.Name = "menuEditCopy";
+            this.menuEditCopy.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.C)));
+            this.menuEditCopy.Size = new System.Drawing.Size(177, 26);
+            this.menuEditCopy.Text = "&Copy";
+            this.menuEditCopy.ToolTipText = "Click to copy the current content.";
+            this.menuEditCopy.Click += new System.EventHandler(this.EditCopyClick);
+            // 
+            // menuEditPaste
+            // 
+            this.menuEditPaste.Image = ((System.Drawing.Image)(resources.GetObject("menuEditPaste.Image")));
+            this.menuEditPaste.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.menuEditPaste.Name = "menuEditPaste";
+            this.menuEditPaste.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.V)));
+            this.menuEditPaste.Size = new System.Drawing.Size(177, 26);
+            this.menuEditPaste.Text = "&Paste";
+            this.menuEditPaste.ToolTipText = "Paste into the clipboard.";
+            this.menuEditPaste.Click += new System.EventHandler(this.EditPasteClick);
+            // 
+            // menuHelp
+            // 
+            this.menuHelp.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.menuHelpAbout});
+            this.menuHelp.Name = "menuHelp";
+            this.menuHelp.Size = new System.Drawing.Size(55, 24);
+            this.menuHelp.Text = "&Help";
+            this.menuHelp.ToolTipText = "Options related to help.";
+            // 
+            // menuHelpAbout
+            // 
+            this.menuHelpAbout.Name = "menuHelpAbout";
+            this.menuHelpAbout.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.H)));
+            this.menuHelpAbout.Size = new System.Drawing.Size(195, 26);
+            this.menuHelpAbout.Text = "&About...";
+            this.menuHelpAbout.ToolTipText = "Click to learn about the application.";
+            this.menuHelpAbout.Click += new System.EventHandler(this.HelpAboutClick);
+            // 
+            // textBoxInput
+            // 
+            this.textBoxInput.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.textBoxInput.Location = new System.Drawing.Point(0, 28);
+            this.textBoxInput.Multiline = true;
+            this.textBoxInput.Name = "textBoxInput";
+            this.textBoxInput.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.textBoxInput.Size = new System.Drawing.Size(800, 422);
+            this.textBoxInput.TabIndex = 1;
+            this.toolTip.SetToolTip(this.textBoxInput, "Enter text here.");
+            // 
+            // formTextEditor
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBoxInput);
+            this.Controls.Add(this.menuTop);
+            this.MainMenuStrip = this.menuTop;
+            this.Name = "formTextEditor";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Text Editor";
+            this.TextChanged += new System.EventHandler(this.TextModified);
+            this.menuTop.ResumeLayout(false);
+            this.menuTop.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuTop;
+        private System.Windows.Forms.ToolStripMenuItem menuFile;
+        private System.Windows.Forms.ToolStripMenuItem newToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem saveToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem saveAsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem editToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem cutToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem copyToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem pasteToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem helpToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem aboutToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem closetoolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem menuFileNew;
+        private System.Windows.Forms.ToolStripMenuItem menuFileOpen;
+        private System.Windows.Forms.ToolStripMenuItem menuFileSave;
+        private System.Windows.Forms.ToolStripMenuItem menuFileSaveAs;
+        private System.Windows.Forms.ToolStripMenuItem menuFileExit;
+        private System.Windows.Forms.ToolStripMenuItem menuEdit;
+        private System.Windows.Forms.ToolStripMenuItem menuEditCut;
+        private System.Windows.Forms.ToolStripMenuItem menuEditCopy;
+        private System.Windows.Forms.ToolStripMenuItem menuEditPaste;
+        private System.Windows.Forms.ToolStripMenuItem menuHelp;
+        private System.Windows.Forms.ToolStripMenuItem menuHelpAbout;
+        private System.Windows.Forms.TextBox textBoxInput;
+        private System.Windows.Forms.ToolTip toolTip;
+    }
+}
+
---
+++ TextEditor/TextEditor.cs
@@ -0,0 +1,219 @@
+﻿// Author: Sonadi Kannnagara
+// Date: 28th March, 2021
+// Type: Lab 5
+// Description: This program allows some functionalities like create, open, save, cut, copy, paste text files.
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TextEditor
+{
+    public partial class formTextEditor : Form
+    {
+        // The file path of the active file.
+        string filePath = String.Empty;       
+
+        public formTextEditor()
+        {
+            InitializeComponent();
+        }
+
+        #region"Event handlers"
+        /// <summary>
+        /// Clear the textbox input, 
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void FileNewClick(object sender, EventArgs e)
+        {
+            // ask the user to confirm.
+            // if the user confirm.
+            if (MessageBox.Show("This will delete the current file. Would you like to proceed?", "Confirm New File", MessageBoxButtons.YesNo) == DialogResult.Yes)
+            {
+                // Clear the textbox.
+                textBoxInput.Clear();
+                // Clear the file path.
+                filePath = String.Empty;
+                // Update the file name.
+                UpdateTitle();
+            }
+        }
+
+        /// <summary>
+        /// Open a text file.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void FileOpenClick(object sender, EventArgs e)
+        {
+            // Create a new open dialog.
+            OpenFileDialog openDialog = new OpenFileDialog();
+            // Set a filetr.
+            openDialog.Filter = "Text files (*.txt)|*.txt|All files (*.*)|*.*";
+            openDialog.Title = "Open file.";
+            // If the user select a file and click ok.
+            if (openDialog.ShowDialog() == DialogResult.OK)
+            {                
+                filePath = openDialog.FileName;
+                FileStream fileToAccess = new FileStream(openDialog.FileName, FileMode.Open, FileAccess.Read);
+                StreamReader fileReader = new StreamReader(fileToAccess);
+                textBoxInput.Text = fileReader.ReadToEnd();
+                fileReader.Close();
+                // Update the file name.
+                UpdateTitle();
+            }
+        }
+
+        /// <summary>
+        /// Save the current content of the text file if the path is known.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void FileSaveClick(object sender, EventArgs e)
+        {
+            // if there is not already a file path.
+            if (filePath == String.Empty)
+            {
+                // Call save as event handler.
+                FileSaveAsClick(sender, e);
+            }
+            //if there is a file path.
+            else
+            {
+                // Save the file.
+                SaveTextFile(filePath);
+            }
+        }
+
+        /// <summary>
+        /// Save the text file to a specified location.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void FileSaveAsClick(object sender, EventArgs e)
+        {
+            // Create a new save dialog.
+            SaveFileDialog saveDialog = new SaveFileDialog();
+            // Set the filter.
+            saveDialog.Filter = "Text files(*.txt)|*.txt|All files(*.*)|*.*";
+            // if the user selects a file and click ok.
+            if(saveDialog.ShowDialog() == DialogResult.OK)
+            {   
+                // Set the new file path.
+                filePath = saveDialog.FileName;
+                // Save the file.
+                SaveTextFile(filePath);
+                // Update the title.
+                UpdateTitle();
+                 
+            }
+        }
+
+        /// <summary>
+        /// Close the application.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void FileExitClick(object sender, EventArgs e)
+        {
+            if (MessageBox.Show("This will close the current file. Would you like to proceed?", "Confirm New File", MessageBoxButtons.YesNo) == DialogResult.Yes)
+            {
+                Close();
+            }                
+        }
+
+        /// <summary>
+        /// Copy the selected content of the text file.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void EditCopyClick(object sender, EventArgs e)
+        {
+            // if the textbox is not null.
+            if (textBoxInput.Text.Length != 0)
+            {
+                Clipboard.SetText(textBoxInput.SelectedText);
+            }
+        }
+
+        /// <summary>
+        /// Cut the selected content from the text file.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void EditCutClick(object sender, EventArgs e)
+        {
+            Clipboard.Clear();
+            if (textBoxInput.Text.Length != 0)
+            {
+                Clipboard.SetText(textBoxInput.SelectedText);
+                textBoxInput.SelectedText = "";
+            }
+        }
+
+        /// <summary>
+        /// Paste the copied content on the text file.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void EditPasteClick(object sender, EventArgs e)
+        {
+            // if the clipboard contains text.
+            if (Clipboard.ContainsText())
+            {   
+                textBoxInput.Text = textBoxInput.Text.Insert(textBoxInput.SelectionStart, Clipboard.GetText());
+                //textBoxInput.SelectedText = Clipboard.GetText();
+            }
+        }
+
+        /// <summary>
+        /// Display some information about the text editor application.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void HelpAboutClick(object sender, EventArgs e)
+        {
+            MessageBox.Show("Text Editor \n By Sonadi Kannangara\n\n 25th March, 2021. \n\n For NETD 2202\n", "About This Application.");
+        }
+#endregion
+        #region "Functions"
+        /// <summary>
+        /// This function will save the content of the text file.
+        /// </summary>
+        /// <param name="path"></param>
+        private void SaveTextFile(string path)
+        {
+            FileStream myFile = new FileStream(path, FileMode.Create, FileAccess.Write);
+            StreamWriter writer = new StreamWriter(myFile);
+            writer.Write(textBoxInput.Text);
+            writer.Close();
+        }
+
+        /// <summary>
+        /// Updates the title of the form to include the path name of the file if one exists.
+        /// </summary>
+        private void UpdateTitle()
+        {
+            this.Text = "Text Editor";
+            if(filePath != String.Empty)
+            {
+                this.Text += " - " + filePath;
+            }
+        }
+
+        #endregion
+
+        private void TextModified(object sender, EventArgs e)
+        {
+           
+        }
+    }
+}
